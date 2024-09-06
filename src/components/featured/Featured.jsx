cb479648e7cc148a9158f34f938b818d60aa6d29
Ali Shibli <alishibli372@gmail.com>
2024-09-06 14:33:42 +0300

    style: Update Featured component styles for responsiveness and initial design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import styles from './Featured.module.css'
+import styles from './featured.module.css'
+import Image from 'next/image'
 const Featured = () => {
     return (
-        <div>Featured</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <b>Ali reporting in</b>—echoes of insight, inspiration, and innovation.
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image
+                        className={`${styles.image}`}
+                        src="/p1.jpeg"
+                        alt="Featured Image"
+                        width={2000}
+                        height={2000}
+                    />
+                </div>
+                <div className={styles.textContainer}>
+                    <h2 className={styles.postTitle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, asperiores?</h2>
+                    <p className={styles.postDesc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis minus nulla culpa soluta, labore odio, nisi laboriosam magni corporis quo architecto a reiciendis dicta magnam aliquid perspiciatis assumenda nobis libero.</p>
+                    <button className={styles.button}>Read More</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
