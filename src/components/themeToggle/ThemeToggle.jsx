b72a9f072d62d18966f4a0f1a7d8da10e2b3a68a
Ali Shibli <alishibli372@gmail.com>
2024-09-18 20:11:44 +0300

    style: Update ThemeToggle component styles for different shape

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -7,10 +7,11 @@ const ThemeToggle = () => {
     const { toggle, theme } = useContext(ThemeContext);
     return (
         <div className={styles.container} onClick={toggle} >
-            <Image src="/moon.png" alt="moon" width={14} height={14} />
-            <div className={styles.ball}></div>
-            <Image src="/sun.png" alt="sun" width={14} height={14} />
-
+            {theme === "dark" ?
+                <Image src="/moon.png" alt="moon" width={14} height={14} />
+                :
+                <Image src="/sun.png" alt="sun" width={14} height={14} />
+            }
         </div>
     )
 }
