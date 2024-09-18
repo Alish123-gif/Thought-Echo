2d65a99d1b166cee2a30fd9a9a4bc88a23c5e13f
Ali Shibli <alishibli372@gmail.com>
2024-09-18 20:09:56 +0300

    style: Update Card component styles for consistency and responsiveness

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,35 +1,30 @@
-import Image from 'next/image'
-import styles from './card.module.css'
-import Link from 'next/link'
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
 
 const Card = () => {
     return (
         <div className={styles.container}>
             <div className={styles.imageContainer}>
-                <Image
-                    className={styles.image}
-                    src="/p1.jpeg"
-                    alt=""
-                    fill />
+                <Image src="/p1.jpeg" alt="Example Image" fill className={styles.image} />
             </div>
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>11.09.2024 - </span>
-                    <span className={`${styles.category} ${styles.coding}`}>Coding</span>
+                    <span className={styles.date}>2023-10-01 - </span>
+                    <span className={styles.category}>coding</span>
                 </div>
-                <h1 className={styles.title}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, nobis?</h1>
-                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Non ipsam perspiciatis exercitationem doloribus eius
-                    tenetur, voluptas minima asperiores repudiandae vel
-                    repellat sapiente, blanditiis quaerat iure possimus
-                    deserunt laboriosam pariatur inventore.
-                </p>
-                <Link className={`${styles.readMore} ${styles.coding}`} href="/">
+                <Link href="/posts/example-slug">
+                    <h1>Example Title</h1>
+                </Link>
+                <div className={styles.desc}>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque nesciunt, aliquam velit unde voluptates quis suscipit deserunt non molestiae quidem aut? Numquam nobis quaerat est quas magni nihil ut sint, quasi corporis voluptatem illo blanditiis fuga facilis, nesciunt non eaque quidem. Quisquam
+                </div>
+                <Link href="/posts/example-slug" className={styles.link}>
                     Read More
                 </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
