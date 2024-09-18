f305d76d4464bf16897f57c30a1d698d5793cdc6
Ali Shibli <alishibli372@gmail.com>
2024-09-18 20:11:03 +0300

    style: Update DropDown component to add social media links in the dropdown menu

--- src/components/navbar/DropDown.jsx
+++ src/components/navbar/DropDown.jsx
@@ -6,84 +6,56 @@ import { TbSocial } from "react-icons/tb";
 import { AuthLinks, ThemeToggle } from '..';
 import Link from 'next/link';
 import Image from 'next/image';
+
 const DropDown = ({ children }) => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
+
     return (
         <>
-            <div className={styles.dropdown}>
+            <div className={styles.iconContainer}>
                 <button onClick={toggleDropdown} className={`${styles.menuButton} ${isDropdownOpen ? styles.open : ''}`}>
                     <TbSocial />
                 </button>
-                {isDropdownOpen && (
-                    <div className={styles.dropdownContent}>
-                        <a
-                            className={styles.dropdownItem}
-                            passHref={true}
-                            href={`/`}>
-                            <Image
-                                src="/x.png"
-                                alt="x"
-                                width={24} height={24}
-                            /><span>X</span>
-                        </a>
-                        <a
-                            className={styles.dropdownItem}
-                            passHref={true}
-                            herf={`/`}>
-                            <Image
-                                src="/instagram.png"
-                                alt="instagram"
-                                width={24} height={24}
-                            /><span>instagram</span>
-                        </a>
-                        <a
-                            className={styles.dropdownItem}
-                            passHref={true}
-                            herf={`/`}>
-                            <Image
-                                src="/linkedin.png"
-                                alt="linkedin"
-                                width={24} height={24}
-                            /><span>linkdIn</span>
-                        </a>
-                        <a
-                            className={styles.dropdownItem}
-                            passHref={true}
-                            herf={`/`}>
-                            <Image
-                                src="/tiktok.png"
-                                alt="tiktok"
-                                width={24} height={24}
-                            /><span>tiktok</span>
-                        </a>
-                    </div>
-                )}
-            </div>
-            {children}
-            <div className={styles.dropdown}>
+                {children}
                 <button
                     onClick={toggleDropdown}
                     className={`${styles.menuButton} ${isDropdownOpen ? styles.open : ''}`}
                 >
                     {isDropdownOpen ? <FaTimes /> : <FaBars />}
                 </button>
-                {isDropdownOpen && (
-                    <div className={`${styles.dropdownContent} ${styles.right}`}>
+            </div>
+
+            {isDropdownOpen && (
+                <div className={styles.dropdown}>
+                    <div className={styles.dropdownContent}>
                         <ThemeToggle className={styles.dropdownItem} />
                         <Link className={styles.dropdownItem} href="/">Home</Link>
                         <Link className={styles.dropdownItem} href="/about">About</Link>
                         <Link className={styles.dropdownItem} href="/contact">Contact</Link>
                         <AuthLinks className={styles.dropdownItem} />
                     </div>
-                )}
-
-            </div>
+                    <div className={styles.dropdownSocail}>
+                        <a className={styles.dropdownItem} href="https://www.facebook.com">
+                            <Image src="/x.png" alt="x" width={24} height={24} /><span>X</span>
+                        </a>
+                        <a className={styles.dropdownItem} href="https://www.instagram.com">
+                            <Image src="/instagram.png" alt="instagram" width={24} height={24} /><span>Instagram</span>
+                        </a>
+                        <a className={styles.dropdownItem} href="https://www.linkedin.com">
+                            <Image src="/linkedin.png" alt="linkedin" width={24} height={24} /><span>LinkedIn</span>
+                        </a>
+                        <a className={styles.dropdownItem} href="https://www.tiktok.com">
+                            <Image src="/tiktok.png" alt="tiktok" width={24} height={24} /><span>TikTok</span>
+                        </a>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
 
-export default DropDown
+export default DropDown;
