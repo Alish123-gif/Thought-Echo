9e40ab5af1e7d4015d933db0be1b36409cda52cb
Ali Shibli <alishibli372@gmail.com>
2025-05-21 14:40:52 +0300

    FEAT: Add UserLinks and AdminLinks exports to index for improved component accessibility

--- Frontend/src/components/index.jsx
+++ Frontend/src/components/index.jsx
@@ -7,3 +7,5 @@ export { default as CategoryList } from './categoryList/CategoryList.jsx';
 export { default as CardList } from './cardList/CardList.jsx';
 export { default as ThemeToggle } from './themeToggle/ThemeToggle.jsx';
 export { default as AuthLinks } from './authLinks/AuthLinks.jsx';
+export { default as AdminLinks } from './adminLinks/AdminLinks.jsx';
+export { default as UserLinks } from './userLinks/UserLinks.jsx';
