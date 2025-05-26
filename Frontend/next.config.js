7e916d1a23bf073b062eea21172d97262afae9d2
Ali Shibli <alishibli372@gmail.com>
2025-05-26 18:48:39 +0300

    Add react-quill dependency for rich text editing support

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     images: {
         domains: ['ik.imagekit.io'],
     },
+    experimental: {
+        appDir: true,
+    },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react-quill/dist/quill.snow.css': require.resolve('react-quill/dist/quill.snow.css'),
+        };
+        return config;
+    },
 };
 
 module.exports = nextConfig;
