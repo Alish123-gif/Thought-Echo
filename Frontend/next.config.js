fbd7c2d7c2f1f755fd3325e6390fb30e58950320
Ali Shibli <alishibli372@gmail.com>
2025-05-26 19:27:08 +0300

    feat: Update next.config.js and package.json for improved build configuration and add netlify.toml for deployment settings

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['ik.imagekit.io'],
-    },
     experimental: {
         appDir: true,
     },
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            'react-quill/dist/quill.snow.css': require.resolve('react-quill/dist/quill.snow.css'),
-        };
-        return config;
+    trailingSlash: true,
+    images: {
+        unoptimized: true
     },
-};
+    output: 'export'
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
