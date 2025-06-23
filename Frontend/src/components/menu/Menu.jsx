fdaa31606d37d073ec9e083128a03ce41af21068
Ali Shibli <alishibli372@gmail.com>
2025-06-23 09:28:16 +0300

    feat: Update Menu component to support initial data for posts and categories

--- Frontend/src/components/menu/Menu.jsx
+++ Frontend/src/components/menu/Menu.jsx
@@ -2,25 +2,35 @@ import React from 'react'
 import styles from './menu.module.css'
 import MenuPosts from '../menuPosts/MenuPosts'
 import MenuCategories from '../menuCatergories/MenuCategories'
-const Menu = () => {
+const Menu = ({ initialData = null }) => {
     return (
         <div className={styles.container}>
             <div className={styles.section}>
                 <h2 className={styles.subtitle}>{"What's hot"}</h2>
                 <h1 className={styles.title}>Most Popular</h1>
-                <MenuPosts withImage={false} type="popular" limit={4} />
+                <MenuPosts
+                    withImage={false}
+                    type="popular"
+                    limit={4}
+                    initialData={initialData?.posts}
+                />
             </div>
 
             <div className={styles.section}>
                 <h2 className={styles.subtitle}>Discover by topic</h2>
                 <h1 className={styles.title}>Categories</h1>
-                <MenuCategories />
+                <MenuCategories initialData={initialData?.categories} />
             </div>
 
             <div className={styles.section}>
                 <h2 className={styles.subtitle}>Chosen by the editor</h2>
                 <h1 className={styles.title}>Editors Pick</h1>
-                <MenuPosts withImage={true} type="featured" limit={4} />
+                <MenuPosts
+                    withImage={true}
+                    type="featured"
+                    limit={4}
+                    initialData={initialData?.posts}
+                />
             </div>
         </div>
     )
