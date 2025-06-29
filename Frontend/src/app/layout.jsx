741397a947cfede7a9cf94ddcbfce77485eba3ad
Ali Shibli <alishibli372@gmail.com>
2025-06-29 15:29:43 +0300

    fix: format layout component for better readability

--- Frontend/src/app/layout.jsx
+++ Frontend/src/app/layout.jsx
@@ -27,7 +27,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>        <ViewportSetter />
+      <body className={inter.className}>
+        <ViewportSetter />
         <PageViewTracker />
         <AuthProvider>
           <CustomAuthProvider>
